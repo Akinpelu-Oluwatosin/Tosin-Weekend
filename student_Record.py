b5a379ee80cfc5b10f6ea27667403792cce99724
Akinpelu Oluwatosin <fortifiedtosin@gmail.com>
2025-09-06 15:40:13 +0100

    Add files via upload

---
+++ student_Record.py
@@ -0,0 +1,11 @@
+studentDetails = {
+    'name': input("Enter your name: "),
+    'age': int(input('How old are you? ')),
+    'City': input('Where do you stay? '),
+    'Department': ["Math",   "Physics",   "Computer Science",   "Biology",   "Chemistry", 
+"Statistics",    "English",    "Economics",    "History",    "Philosophy", 
+"Sociology",   "Political Science",    "Geography",   "Psychology",   "Art", 
+"Music",   "Engineering",   "Law",    "Medicine",    "Business".
+]
+}
+
